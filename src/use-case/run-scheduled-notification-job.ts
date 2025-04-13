91c1c986f1237ce7f8c7544fc46b79f6bffbd778
futakuchi-eitaro <futakuchi-eitaro@dmm.com>
2025-04-13 22:54:06 +0900

    docs: `runScheduledNotificationJob`のコメント追加

--- src/use-case/run-scheduled-notification-job.ts
+++ src/use-case/run-scheduled-notification-job.ts
@@ -17,15 +17,18 @@ import { Confluence } from "~/types";
  * @returns {Promise<void>} 処理が完了したら解決される Promise
  */
 export async function runScheduledNotificationJob() {
+  // 前回実行時のタイムスタンプを読み取る（存在しない場合は null）
   const pollingInfo = parsePollingInfo();
-  // 引数指定した日時以降に更新されたページ一覧を取得
+
+  // タイムスタンプ以降に更新されたページ一覧を取得（最大 limit 件まで）
   const recentChangePages = await fetchRecentChanges(pollingInfo?.timestamp);
   let { results: searchResults, _links: links } = recentChangePages;
 
-  // 一度に取得できない場合は、ループして取得
-  let nextEndpoint = links.next;
+  // 検索結果が複数ページに渡る場合、すべてのページをループで取得する
+  let nextEndpoint = links?.next;
   while (nextEndpoint) {
     const nextPages = await fetchConfluenceApi<Confluence.SearchPage>(nextEndpoint);
+    // 結果を蓄積
     searchResults = [...searchResults, ...nextPages.results];
     nextEndpoint = nextPages._links.next;
   }
@@ -34,5 +37,7 @@ export async function runScheduledNotificationJob() {
     const payload = convertSearchResultToMessagePayload(result, links.base);
     await sendSlackMessage(payload);
   });
+
+  // 最後に今回の実行時刻を保存し、次回以降の差分取得に備える
   updatePollingInfo();
 }
